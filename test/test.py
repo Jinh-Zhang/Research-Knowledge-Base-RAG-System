357fa0115a2efac09694465fb940a15204cb01ab
111 <1223113675@qq.com>
2026-05-25 10:23:57 +0800

    remove test

--- test/test.py
+++
@@ -1,8 +0,0 @@
-from pymilvus import MilvusClient
-client = MilvusClient(uri="http://127.0.0.1:19530")
-result = client.query(
-    collection_name="paper_item_names_v2",
-    filter='paper_title == "Backpropagation-Free Test-Time Adaptation via Probabilistic Gaussian Alignment"',
-    output_fields=["paper_title", "file_title"]
-)
-print(result)
